6863ce2fa9f619f35ed20572a01fbf47546503da
djw319 <64276595+djw319@users.noreply.github.com>
2020-12-06 17:51:53 -0500

    Add video playback loop

--- Fireplace.py
+++ Fireplace.py
@@ -3,4 +3,10 @@
 from omxplayer.player import OMXPlayer
 from pathlib import Path
 
-fireplace_video = OMXPlayer('/home/pi/Digital-Fireplace/fireplace_vid', args=['--loop'])
+fireplace_video = OMXPlayer('/home/pi/Digital-Fireplace/fireplace_vid.mp4', args=['--loop'])
+
+def main():
+    while True:
+        print('playing back video')
+        fireplace_video.play()
+        
